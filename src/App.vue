999c3b91d47cbf4d477e36dbc63188a2307b14e9
Dima Lamkin <dlamkin@s-telematics.ru>
2022-12-02 00:50:32 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <Test />
+    </div>
+</template>
+
+<script>
+import Test from './Test.vue'
+
+import { onMounted } from 'vue'
+import { useTest } from 'lib-test/src/lib'
+import { useTestLocal } from './composable'
+export default {
+    name: 'App',
+    components: {
+        Test
+    },
+
+    data() {
+        return {};
+    },
+
+    setup() {
+        console.log("APP:SETUP")
+
+        onMounted(() => {
+            console.log("APP:onMounted")
+        })
+        useTestLocal();
+        useTest()
+    },
+
+    created() {
+        // console.log("APP")
+    }
+};
+</script>
---
+++ src/Test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>Test</div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+console.log("TEST:SETUP")
+
+onMounted(() => {
+    console.log("Test:mounted")
+})
+</script>
+
+<!-- <script>
+import { onMounted } from 'vue'
+
+export default {
+    setup() {
+        onMounted(() => {
+            console.log("mounted")
+        })
+    }
+}
+
+</script> -->
